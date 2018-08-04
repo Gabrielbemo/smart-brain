3ccea33a7308c993f9dba4deed92fd3becd02e4f
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-04 12:54:41 -0300

    update de segurança no login e no registro

--- src/App.js
+++ src/App.js
@@ -7,13 +7,9 @@ import Register from './components/Register/Register';
 import Signin from './components/Signin/Signin';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+
 
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: 'd3467d5210d246a18c926188c9cc734b'
-});
 
 const particlesOptions = {
   particles: {
@@ -87,7 +83,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
@@ -101,6 +104,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
